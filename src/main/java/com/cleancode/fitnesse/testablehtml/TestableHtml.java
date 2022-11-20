22e98a2845e57b671f4a02011c54e2cab3c06a06
rina <rina4674@naver.com>
2022-11-20 16:44:11 +0900

    8번째 과제

--- src/main/java/com/cleancode/fitnesse/testablehtml/TestableHtml.java
+++ src/main/java/com/cleancode/fitnesse/testablehtml/TestableHtml.java
@@ -5,46 +5,50 @@ import fitnesse.wiki.*;
 
 public class TestableHtml {
 
+    // 중복되는 문자열을 상수로 만들어 사용
+    public static final String INCLUDE_SETUP = "!include -setup .";
+    public static final String INCLUDE_TEARDOWN = "!include -teardown .";
+    static StringBuffer buffer;
+
     public String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
         WikiPage wikiPage = pageData.getWikiPage();
-        StringBuffer buffer = new StringBuffer();
+        buffer = new StringBuffer();
+        boolean isPageData = pageData.hasAttribute("Test"); // if문의 조건을 boolean으로 따로 만들어 짧게 줄인다.
 
-        if (pageData.hasAttribute("Test")) {
-            if (includeSuiteSetup) {
-                WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage);
-                if (suiteSetup != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteSetup);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -setup .").append(pagePathName).append("\n");
-                }
-            }
+        if (isPageData) {
+            isIncludeSuiteSetup(includeSuiteSetup, SuiteResponder.SUITE_SETUP_NAME, wikiPage, INCLUDE_SETUP);
             WikiPage setup = PageCrawlerImpl.getInheritedPage("SetUp", wikiPage);
-            if (setup != null) {
-                WikiPagePath setupPath = wikiPage.getPageCrawler().getFullPath(setup);
-                String setupPathName = PathParser.render(setupPath);
-                buffer.append("!include -setup .").append(setupPathName).append("\n");
-            }
+            isSetupNull(setup, wikiPage, INCLUDE_SETUP);
         }
 
         buffer.append(pageData.getContent());
-        if (pageData.hasAttribute("Test")) {
+        if (isPageData) {
             WikiPage teardown = PageCrawlerImpl.getInheritedPage("TearDown", wikiPage);
-            if (teardown != null) {
-                WikiPagePath tearDownPath = wikiPage.getPageCrawler().getFullPath(teardown);
-                String tearDownPathName = PathParser.render(tearDownPath);
-                buffer.append("!include -teardown .").append(tearDownPathName).append("\n");
-            }
-            if (includeSuiteSetup) {
-                WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(SuiteResponder.SUITE_TEARDOWN_NAME, wikiPage);
-                if (suiteTeardown != null) {
-                    WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteTeardown);
-                    String pagePathName = PathParser.render(pagePath);
-                    buffer.append("!include -teardown .").append(pagePathName).append("\n");
-                }
-            }
+            isSetupNull(teardown, wikiPage, INCLUDE_TEARDOWN);
+            isIncludeSuiteSetup(includeSuiteSetup, SuiteResponder.SUITE_TEARDOWN_NAME, wikiPage, INCLUDE_TEARDOWN);
         }
 
         pageData.setContent(buffer.toString());
         return pageData.getHtml();
+    }
+
+    private static void isIncludeSuiteSetup(boolean includeSuiteSetup, String suiteSetupName, WikiPage wikiPage, String includeSetup) throws Exception {
+        if (includeSuiteSetup) {
+            WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(suiteSetupName, wikiPage);
+            isSetupNull(suiteSetup, wikiPage, includeSetup);
+        }
+    }
+
+    private static void isSetupNull(WikiPage setup, WikiPage wikiPage, String includeSetup) throws Exception {
+        if (setup != null) {
+            getPathName(wikiPage, setup, includeSetup);
+        }
+    }
+
+    //중복되는 구문들을 하나의 메소드로 추출하여 사용
+    private static void getPathName(WikiPage wikiPage, WikiPage suiteTeardown, String includeTeardown) throws Exception {
+        WikiPagePath pagePath = wikiPage.getPageCrawler().getFullPath(suiteTeardown); // 중복되는 내용이지만 이름이 각기 다르게 설정되어 여러개 사용되고 있던 것을 합쳤다.
+        String pathName = PathParser.render(pagePath);
+        buffer.append(includeTeardown + pathName + "\n");
     }
 }
